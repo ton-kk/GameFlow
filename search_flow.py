9f975987b2ef34f04f7e601bb351158e13661c74
ton-kk <58809086+ton-kk@users.noreply.github.com>
2021-05-01 00:26:35 +0900

    commit

---
+++ search_flow.py
@@ -0,0 +1,23 @@
+# character skill seeds.
+# player or main character or sub or onother
+seeds = []
+
+# status
+# physical: name age sex shape strength agility endurance dexterity intelligence luck
+# point: hitpoint hunger thirst fatige
+# skills: <<may be boolean dictionary>>
+class character:
+    profile = {}
+    def __init__(self, name, priority):
+
+        print("charcter created!")
+
+
+
+
+def main():
+
+
+
+if __name__ == '__main__':
+    main()
